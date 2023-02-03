5958cba33f72c4429271db93ef826e7700bde0cb
S R Rayhan <shafikulrahman66@gmail.com>
2023-02-04 00:29:05 +0600

    Solve the problem of login each and every time

--- app/src/main/java/com/example/officeroom/MainActivity.java
+++ app/src/main/java/com/example/officeroom/MainActivity.java
@@ -1,10 +1,9 @@
 package com.example.officeroom;
 
-import androidx.annotation.ColorInt;
+import androidx.annotation.MainThread;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +13,7 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     private EditText email;
@@ -65,5 +65,15 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user!=null){
+            startActivity(new Intent(MainActivity.this,HomeActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK));
+            finish();
+        }
     }
 }
